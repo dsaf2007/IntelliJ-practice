ce04946e0dffce71bf3c9e20693e224556fb0bb3
dsaf2007 <dsaf2007@naver.com>
2023-04-19 13:20:01 +0900

    first commit

---
+++ src/main/java/org/example/PrintHello.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class PrintHello {
+
+    public static void printHello() {
+        System.out.println(1 + 1);
+    }
+
+    public static void printBye() {
+        System.out.println("Bye!");
+    }
+
+    public static void main(String[] args) {
+
+        printHello();
+        printBye();
+    }
+}
