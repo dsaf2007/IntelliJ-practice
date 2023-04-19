63b97ece82cf62f364214cb3f80d51cdc4a03184
dsaf2007 <dsaf2007@naver.com>
2023-04-19 15:27:25 +0900

    feat : Scanner을 이용해서 두 숫자를 입력 받고 더한 결과를 출력

---
+++ src/main/java/org/example/ScannerEx.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTwoNumbers(int val1, int val2) {
+
+    }
+
+    public void readTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/org/example/ScannerExTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
