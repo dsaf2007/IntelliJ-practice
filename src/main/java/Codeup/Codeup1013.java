a3ab576c9643086dd510400fe520313148ebdb94
dsaf2007 <dsaf2007@naver.com>
2023-04-20 13:47:37 +0900

    Codeup 1013

---
+++ src/main/java/Codeup/Codeup1013.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Codeup1013 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.printf("%d %d", a, b);
+    }
+}
--- src/main/java/org/example/ScannerEx.java
+++ src/main/java/org/example/ScannerEx.java
@@ -7,13 +7,25 @@ public class ScannerEx {
     private String val1;
     private String val2;
 
+    Scanner sc = new Scanner(System.in);
+
     public void readTwoStrings() {
-        Scanner sc = new Scanner(System.in);
+
         this.val1 = sc.next();
         this.val2 = sc.next();
     }
 
     public String addTwoStrings() {
         return this.val1 + this.val2;
+    }
+
+    public double getAverage()
+    {
+        double num1; double num2; double num3;
+
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+        num3 = sc.nextDouble();
+        return (double)((num1 + num2 + num3)/(3.0));
     }
 }
