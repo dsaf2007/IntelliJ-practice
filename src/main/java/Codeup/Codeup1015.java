aa0a087bb0aa03d4072cebdad1037bd029efe125
dsaf2007 <dsaf2007@naver.com>
2023-04-20 17:35:27 +0900

    Codeup 1015

---
+++ src/main/java/Codeup/Codeup1015.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        float num1;
+        Scanner sc = new Scanner(System.in);
+
+        num1 = sc.nextFloat();
+        num1 = (float) (Math.round(num1 * 100)/100.0);
+
+        System.out.printf("%.2f",num1);
+    }
+}
