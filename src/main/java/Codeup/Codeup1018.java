6caf77e0c82a6d33ecfcd8d1b519e6ce949df0da
dsaf2007 <dsaf2007@naver.com>
2023-04-21 11:03:32 +0900

    Codeup 1018

---
+++ src/main/java/Codeup/Codeup1018.java
@@ -0,0 +1,12 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String inputStr = sc.nextLine();
+        System.out.println(inputStr);
+    }
+}
