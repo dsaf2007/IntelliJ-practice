ce6d61038c613d2dd9b7a899d8cd79aef677038a
dsaf2007 <dsaf2007@naver.com>
2023-04-24 15:38:01 +0900

    feat : Codeup 1028-30

---
+++ src/main/java/codeup/Codeup1030.java
@@ -0,0 +1,13 @@
+﻿package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num1 = sc.nextLong();
+
+        System.out.println(num1);
+    }
+}
---
+++ src/main/java/codeup/Codeup2019.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup2019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = sc.nextDouble();
+
+        System.out.printf("%.11f",num1);
+    }
+}
