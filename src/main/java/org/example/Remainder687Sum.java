c1cf5e13aa1fb0800924369d74164ad3afa872c8
dsaf2007 <dsaf2007@naver.com>
2023-04-26 15:48:27 +0900

    feat : 누적 연산 활용

--- src/main/java/org/example/Remainder687Sum.java
+++ src/main/java/org/example/Remainder687Sum.java
@@ -3,11 +3,12 @@ package org.example;
 public class Remainder687Sum {
     public static void main(String[] args) {
         int num = 687;
-        int a = num % 10;
+        int answer = 0;
+        answer += num % 10;
         num /= 10;
-        int b = num % 10;
+        answer += num % 10;
         num /= 10;
-        int c = num %10;
-        System.out.println(a+b+c);
+        answer += num % 10;
+        System.out.println(answer);
     }
 }
