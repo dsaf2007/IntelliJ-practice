a7a97db7642e70cc1e9e9792950fe7c6b15da3ff
dsaf2007 <dsaf2007@naver.com>
2023-04-27 13:33:46 +0900

    feat : ~ Codeup1058

---
+++ src/main/java/codeup/Codeup1053.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+
+        System.out.println(b == 1 ? 0 : 1);
+    }
+}
