0ecadcc9e3e2f8459f935e1235c08b2ecf20f3d9
dsaf2007 <dsaf2007@naver.com>
2023-04-27 15:11:55 +0900

    feat : ~ Codeup1062

--- src/main/java/codeup/Codeup1062.java
+++ src/main/java/codeup/Codeup1062.java
@@ -6,7 +6,6 @@ public class Codeup1062 {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-
         System.out.println(num1^num2);
     }
 }
